78b5fec2fd3770461798c4ae1f1ecbd49431cfe6
Jani Perčič <jani.percic@gmail.com>
2019-08-21 11:03:22 +0200

    Generated skeleton from open-api.yaml and started work on implementing endpoints

---
+++ lib/Api/DefaultApi.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace OpenAPIServer\Api;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class DefaultApi extends AbstractDefaultApi
+{
+    public function questionsGet(ServerRequestInterface $request, ResponseInterface $response, array $args)
+    {
+
+    }
+
+    public function questionsPost(ServerRequestInterface $request, ResponseInterface $response, array $args)
+    {
+
+    }
+
+}
---
+++ lib/Controller/QuestionController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace OpenAPIServer\Controller;
+
+class QuestionController
+{
+
+}
